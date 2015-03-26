84581f94e349c2a2adf7ac7a2707052e2d9bb61c
CumbersomeFugly <cumbersomefugly@gmail.com>
2015-03-26 16:35:03 -0500

    Almost working version of learnYouNode13.

--- learnYouNode13.js
+++ learnYouNode13.js
@@ -1,13 +1,54 @@
-var http = require('http'),
-    map = require('through2-map');
+var http = require('http');
+
+var timeParser = function (data) {
+
+    var expectsUNIX = false,
+        convertedDate = "",
+        convertedYears = 0,
+        convertedMonths = 0,
+        convertedDays = 0,
+        convertedHours = 0,
+        convertedMinutes = 0,
+        convertedSeconds = 0,
+        convertedMilliseconds = 0;
+
+    if (data.charAt(5) === "p") {
+        convertedDate = data.slice(19, data.length);
+    } else { 
+        convertedDate = data.slice(18, data.length);
+        expectsUNIX= true;
+    }
+
+    convertedYears = Number(convertedDate.slice(0,4));
+    convertedMonths = Number(convertedDate.slice(5,7));
+    convertedDays = Number(convertedDate.slice(8,10));
+    convertedHours = Number(convertedDate.slice(11,13) -5);
+    convertedMinutes = Number(convertedDate.slice(14,16));
+    convertedSeconds = Number(convertedDate.slice(17,19));
+    convertedMilliseconds = Number(convertedDate.slice (20,23));
+
+//making date file
+    if (expectsUNIX === true) {
+        var date = new Date(convertedYears, convertedMonths - 1, convertedDays, convertedMinutes, convertedSeconds, convertedMilliseconds);
+        return {"unixtime": date.getTime()};
+    } else {
+    //making JSON file
+        var JSONformatted = {
+            "hour": Number(convertedHours),
+            "minute": Number(convertedMinutes),
+            "second": Number(convertedSeconds)
+        }
+    
+        return JSONformatted;
+    }
+}
 
 var callback = function (request, response) {
-    request.pipe(map(function (chunk) {
-        return chunk.toString().toUpperCase();
-    })).pipe(response);
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(timeParser(request.url)));
+    response.end();
 }
 
 var server = http.createServer(callback);
 
 server.listen(process.argv[2]);
-
